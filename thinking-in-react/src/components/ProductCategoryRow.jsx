5efe36329d269958d2e9801ae1502be72faadae1
BEN FRAJ Farouk <farouk.ben-fraj@epitech.eu>
2019-06-24 12:57:47 +0200

    static front

---
+++ thinking-in-react/src/components/ProductCategoryRow.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export function ProductCategoryRow(props) {
+    const category = props.category;
+    return(
+        <tr>
+            <th colSpan='2'>
+                {category}
+            </th>
+        </tr>
+    );
+}
+
+export default ProductCategoryRow;
